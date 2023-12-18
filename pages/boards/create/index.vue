08e5b5ef5f3c4a0bebe23e29237d78dd555cf3be
unknown <shlee@idevel.kr>
2023-12-18 18:01:27 +0900

    Feat : create 페이지

---
+++ pages/boards/create/index.vue
@@ -0,0 +1,15 @@
+<script>
+import { BoardForm } from "#build/components";
+export default {
+  components: {
+    BoardForm,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>게시글 등록</h1>
+    <BoardForm />
+  </div>
+</template>
