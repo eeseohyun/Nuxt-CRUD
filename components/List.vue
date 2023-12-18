9084d92f96d78da4cbc0ae933a689b104b874166
unknown <shlee@idevel.kr>
2023-12-18 17:59:09 +0900

    Feat : List 컴포넌트

---
+++ components/List.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  props: {
+    boards: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <div v-for="(item, index) in boards" :key="item.id" class="mt-2">
+    <slot :item="item" :index="index"></slot>
+  </div>
+</template>
+
+<style></style>
