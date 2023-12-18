4ef1aa9b6254b641bc4ce5c0f23459a1970cf21a
unknown <shlee@idevel.kr>
2023-12-18 18:00:13 +0900

    Feat : Toast 컴포넌트

---
+++ components/Toast.vue
@@ -0,0 +1,14 @@
+<template>
+  <UNotification
+    description="{{toast.message}}"
+    :id="2"
+    :timeout="0"
+    title="Notification"
+  />
+</template>
+
+<script setup>
+const toast = useToast();
+</script>
+
+<style></style>
