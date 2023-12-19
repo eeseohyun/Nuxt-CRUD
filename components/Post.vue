22fb662681ab9dbea895643985d04660314f82c7
eeseohyun <jinlijinyoung@gmail.com>
2023-12-20 01:42:17 +0900

    Feat: Post component UI

--- components/Post.vue
+++ components/Post.vue
@@ -1,13 +1,32 @@
 <template>
-  <div class="post">
-    <h3>{{ post.title }}</h3>
-    <p>{{ post.body }}</p>
-  </div>
+  <tr>
+    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+      <p class="text-gray-900 whitespace-no-wrap">{{ post.id }}</p>
+    </td>
+    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+      <NuxtLink to="`/boards/${post.id}`"
+        ><p class="text-gray-900 whitespace-no-wrap">
+          {{ post.title }}
+        </p></NuxtLink
+      >
+    </td>
+    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+      <p class="text-gray-900 whitespace-no-wrap">{{ post.updatedDate }}</p>
+    </td>
+    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+      <span
+        class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight"
+      >
+        <span
+          aria-hidden
+          class="absolute inset-0 bg-green-200 opacity-50 rounded-full"
+        ></span>
+        <span class="relative">Activo</span>
+      </span>
+    </td>
+  </tr>
 </template>
-<script>
-export default {
-  props: ['post'],
-  setup(props) {},
-};
+<script setup>
+const post = ref({});
 </script>
 <style></style>
