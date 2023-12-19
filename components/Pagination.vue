dd7eaca12322bfc4202db46a72b1f2501cf0a0ea
eeseohyun <jinlijinyoung@gmail.com>
2023-12-20 01:41:49 +0900

    Feat: Pagination component

---
+++ components/Pagination.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between"
+  >
+    <span class="text-xs xs:text-sm text-gray-900"> 1 / 50 </span>
+    <div class="inline-flex mt-2 xs:mt-0">
+      <button
+        class="text-sm text-indigo-50 transition duration-150 hover:bg-lime-600 bg-lime-500 font-semibold py-2 px-4 rounded-l"
+      >
+        ﹤
+      </button>
+      &nbsp; &nbsp;
+      <button
+        class="text-sm text-indigo-50 transition duration-150 hover:bg-lime-600 bg-lime-500 font-semibold py-2 px-4 rounded-r"
+      >
+        ﹥
+      </button>
+    </div>
+  </div>
+</template>
+<script setup></script>
+<style></style>
