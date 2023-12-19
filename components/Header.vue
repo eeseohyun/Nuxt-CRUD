6060d1c6ea2449aeb8ec2d886a496655193317b4
eeseohyun <jinlijinyoung@gmail.com>
2023-12-20 01:41:30 +0900

    Feat: Header ui 수정

--- components/Header.vue
+++ components/Header.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
-    <nav class="flex items-center justify-between flex-wrap bg-lime-500 p-6">
+    <nav class="flex items-center flex-wrap bg-lime-500 p-6">
       <div class="flex items-center flex-shrink-0 text-white mr-6">
-        <span class="font-semibold text-xl">공지사항</span>
+        <NuxtLink to="/"
+          ><span class="font-semibold text-xl">Idevel</span></NuxtLink
+        >
+      </div>
+      <div class="flex items-center flex-shrink-0 text-white mr-6">
+        <NuxtLink to="/boards"><span class="text-md">공지사항</span></NuxtLink>
       </div>
     </nav>
     <slot></slot>
   </div>
 </template>
-<script>
-export default {};
-</script>
+<script setup></script>
 <style></style>
