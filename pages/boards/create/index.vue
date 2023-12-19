9f49cd679b2f63cdb15c2924be4100183535a1fa
eeseohyun <jinlijinyoung@gmail.com>
2023-12-20 01:44:34 +0900

    Feat: Board Create Page UI

--- pages/boards/create/index.vue
+++ pages/boards/create/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <form class="flex min-w-max h-[100vh] justify-center">
-    <div class="space-y-12 h-full">
+  <form class="flex flex-col max-w-md mx-auto justify-center">
+    <div class="w-full">
       <div class="border-b border-gray-900/10 pb-12">
         <h2
           class="text-xl font-semibold leading-7 text-gray-900 flex justify-center items-centers p-3"
@@ -8,16 +8,16 @@
           게시글 등록
         </h2>
 
-        <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-          <div class="sm:col-span-4">
+        <div class="mt-10">
+          <div>
             <label
-              for="username"
+              for="title"
               class="block text-sm font-medium leading-6 text-gray-900"
               >제목</label
             >
             <div class="mt-2">
               <div
-                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
+                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-lime-400 sm:max-w-md"
               >
                 <input
                   type="text"
@@ -30,68 +30,50 @@
             </div>
           </div>
 
-          <div class="col-span-full">
+          <div class="mt-4">
             <label
-              for="about"
+              for="body"
               class="block text-sm font-medium leading-6 text-gray-900"
               >내용</label
             >
             <div class="mt-2">
               <textarea
-                id="about"
-                name="about"
-                rows="3"
-                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                id="body"
+                name="body"
+                rows="10"
+                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lime-400 sm:text-sm sm:leading-6"
               />
-            </div>
-          </div>
-
-          <div class="col-span-full">
-            <label
-              for="cover-photo"
-              class="block text-sm font-medium leading-6 text-gray-900"
-              >Cover photo</label
-            >
-            <div
-              class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10"
-            >
-              <div class="text-center">
-                <PhotoIcon
-                  class="mx-auto h-12 w-12 text-gray-300"
-                  aria-hidden="true"
-                />
-                <div class="mt-4 flex text-sm leading-6 text-gray-600">
-                  <label
-                    for="file-upload"
-                    class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
-                  >
-                    <span>Upload a file</span>
-                    <input
-                      id="file-upload"
-                      name="file-upload"
-                      type="file"
-                      class="sr-only"
-                    />
-                  </label>
-                  <p class="pl-1">or drag and drop</p>
-                </div>
-                <p class="text-xs leading-5 text-gray-600">
-                  PNG, JPG, GIF up to 10MB
-                </p>
-              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
+    <div>
+      <label for="tags">
+        <div>
+          <div>
+            <input
+              type="text"
+              name="tags"
+              id="tags"
+              autocomplete="tags"
+              v-model="tags"
+              class="block flex-1 border-0 bg-transparent py-1.5"
+            />
+          </div>
+        </div>
+      </label>
+    </div>
     <div class="mt-6 flex items-center justify-end gap-x-6">
       <button
         type="button"
         class="text-sm font-semibold leading-6 text-gray-900"
+        @click="cancelPost"
       >
         취소
       </button>
       <button
+        @click.prevent="submitPost"
         type="submit"
         class="rounded-md bg-lime-400 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-lime-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
       >
@@ -101,7 +83,33 @@
   </form>
 </template>
 
-<script>
-export default {};
+<script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const title = ref('');
+const body = ref('');
+const tags = ref('');
+
+const cancelPost = () => {
+  router.go(-1);
+};
+
+const submitPost = async () => {
+  const currentDate = new Date().toISOString().split('T')[0];
+  try {
+    const response = await useFetch('http://localhost:3000/posts', {
+      title: title.value,
+      body: body.value,
+      updatedDate: currentDate,
+      tags: tags.value.split(',').map((tag) => tag.trim()),
+    });
+    alert('게시글이 등록되었습니다!');
+  } catch (error) {
+    console.log(error);
+    alert('게시글 등록에 실패하였습니다. 다시 시도해주세요.');
+  }
+};
 </script>
 <style></style>
