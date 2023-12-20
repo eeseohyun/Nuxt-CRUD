fd018f470f1837cf0cd8e390e58282d498b44d34
jinlijinyoung <shlee@idevel.kr>
2023-12-20 17:57:06 +0900

    Style: 변수명 변경

--- utils/getPosts.js
+++ utils/getPosts.js
@@ -1,18 +1,18 @@
-import { ref } from 'vue';
-import axios from 'axios';
+import { ref } from "vue";
+import axios from "axios";
 
 const getPosts = () => {
   const posts = ref([]);
   const error = ref(null);
   const load = async () => {
     try {
-      let data = await useFetch('http://localhost:3000/posts');
-      if (!data.ok) {
-        throw Error('데이터를 읽어올 수 없습니다!');
+      let response = await useFetch("http://localhost:3000/posts");
+      if (!response.ok) {
+        throw Error("⚠️ 데이터를 읽어올 수 없습니다!");
       }
-      posts.value = await data.json();
+      posts.value = await response.json();
+
       console.log(posts.value);
-      console.log(data);
     } catch (err) {
       error.value = err.message;
     }
