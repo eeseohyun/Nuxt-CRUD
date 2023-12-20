6aaf935bfdd867e3afca07831f4c5d9a708a1e84
eeseohyun <jinlijinyoung@gmail.com>
2023-12-21 01:38:50 +0900

    Fix: 기능 버그 및 에러 수정

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -2,15 +2,19 @@
   <div
     class="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between"
   >
-    <span class="text-xs xs:text-sm text-gray-900"> 1 / 50 </span>
+    <span class="text-xs xs:text-sm text-gray-900">
+      {{ currentPage }} / {{ totalPages }}
+    </span>
     <div class="inline-flex mt-2 xs:mt-0">
       <button
+        @click.prevent="goToPrev"
         class="text-sm text-indigo-50 transition duration-150 hover:bg-lime-600 bg-lime-500 font-semibold py-2 px-4 rounded-l"
       >
         ﹤
       </button>
       &nbsp; &nbsp;
       <button
+        @click.prevent="goToNext"
         class="text-sm text-indigo-50 transition duration-150 hover:bg-lime-600 bg-lime-500 font-semibold py-2 px-4 rounded-r"
       >
         ﹥
@@ -18,5 +22,7 @@
     </div>
   </div>
 </template>
-<script setup></script>
+<script setup>
+const { goToNext, goToPrev } = defineProps(['goToNext', 'goToPrev']);
+</script>
 <style></style>
