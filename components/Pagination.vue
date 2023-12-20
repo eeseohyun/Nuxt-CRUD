bec7b9e247b6536a53f998be036aa4255fece0ed
unknown <shlee@idevel.kr>
2023-12-20 17:25:07 +0900

    Feat: Pagination 구현

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -2,15 +2,17 @@
   <div
     class="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between"
   >
-    <span class="text-xs xs:text-sm text-gray-900"> 1 / 50 </span>
+    <span class="text-xs xs:text-sm text-gray-900">{{ currentPage }}</span>
     <div class="inline-flex mt-2 xs:mt-0">
       <button
+        @click.prevent="goToPrev()"
         class="text-sm text-indigo-50 transition duration-150 hover:bg-lime-600 bg-lime-500 font-semibold py-2 px-4 rounded-l"
       >
         ﹤
       </button>
       &nbsp; &nbsp;
       <button
+        @click.prevent="goToNext()"
         class="text-sm text-indigo-50 transition duration-150 hover:bg-lime-600 bg-lime-500 font-semibold py-2 px-4 rounded-r"
       >
         ﹥
@@ -18,5 +20,28 @@
     </div>
   </div>
 </template>
-<script setup></script>
+<script setup>
+import { defineProps } from "vue";
+defineProps({
+  posts: { type: Array, required: true },
+  currentPage: { type: Number, required: true },
+  perPage: { type: Number, required: true },
+});
+
+const allPages = computed(() => {
+  return posts.length;
+});
+
+const goToPrev = () => {
+  if (currentPage.value !== 1) {
+    currentPage.value--;
+  }
+};
+
+const goToNext = () => {
+  if (currentPage.value !== Math.ceil(posts.length / perPage)) {
+    currentPage.value++;
+  }
+};
+</script>
 <style></style>
