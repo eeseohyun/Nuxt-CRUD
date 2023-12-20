4035bb348d45a0dd6afe9b5e43d717a318c872fd
jinlijinyoung <shlee@idevel.kr>
2023-12-20 17:51:52 +0900

    Feat: 렌더링 조건추가

--- components/Post.vue
+++ components/Post.vue
@@ -1,10 +1,12 @@
 <template>
+  <div v-if="isLoading"><Loader /></div>
+
   <tr>
     <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
       <p class="text-gray-900 whitespace-no-wrap">{{ post.id }}</p>
     </td>
     <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-      <NuxtLink to="`/boards/${post.id}`"
+      <NuxtLink to="/boards/{{post.id}}"
         ><p class="text-gray-900 whitespace-no-wrap">
           {{ post.title }}
         </p></NuxtLink
@@ -14,19 +16,70 @@
       <p class="text-gray-900 whitespace-no-wrap">{{ post.updatedDate }}</p>
     </td>
     <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-      <span
-        class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight"
-      >
+      <div v-if="post?.tags === 'BE'">
         <span
-          aria-hidden
-          class="absolute inset-0 bg-green-200 opacity-50 rounded-full"
-        ></span>
-        <span class="relative">Activo</span>
-      </span>
+          class="relative inline-block px-3 py-1 font-semibold text-blue-900 leading-tight"
+        >
+          <span
+            aria-hidden
+            class="absolute inset-0 bg-blue-200 opacity-50 rounded-full"
+          ></span>
+          <span class="relative">{{ post.tags }}</span>
+        </span>
+      </div>
+      <div v-if="post?.tags === 'FE'">
+        <span
+          class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight"
+        >
+          <span
+            aria-hidden
+            class="absolute inset-0 bg-green-200 opacity-50 rounded-full"
+          ></span>
+          <span class="relative">{{ post.tags }}</span>
+        </span>
+      </div>
+      <div v-if="post?.tags === 'design'">
+        <span
+          class="relative inline-block px-3 py-1 font-semibold text-orange-900 leading-tight"
+        >
+          <span
+            aria-hidden
+            class="absolute inset-0 bg-orange-200 opacity-50 rounded-full"
+          ></span>
+          <span class="relative">{{ post.tags }}</span>
+        </span>
+      </div>
+      <div v-if="post?.tags === 'plan'">
+        <span
+          class="relative inline-block px-3 py-1 font-semibold text-red-900 leading-tight"
+        >
+          <span
+            aria-hidden
+            class="absolute inset-0 bg-red-200 opacity-50 rounded-full"
+          ></span>
+          <span class="relative">{{ post.tags }}</span>
+        </span>
+      </div>
+      <div v-if="post?.tags === 'all'">
+        <span
+          class="relative inline-block px-3 py-1 font-semibold text-gray-900 leading-tight"
+        >
+          <span
+            aria-hidden
+            class="absolute inset-0 bg-gray-200 opacity-50 rounded-full"
+          ></span>
+          <span class="relative">{{ post.tags }}</span>
+        </span>
+      </div>
     </td>
   </tr>
 </template>
 <script setup>
-const post = ref({});
+import { defineProps } from "vue";
+import Loader from "./Loader.vue";
+const props = defineProps({
+  post: { type: Object, required: true },
+  isLoading: { type: Boolean, required: true },
+});
 </script>
 <style></style>
