2e8d906558fbf6453d33eee274519e5fc234c7a4
jinlijinyoung <shlee@idevel.kr>
2023-12-21 18:19:17 +0900

    Style: 코드 수정

--- components/PostList.vue
+++ components/PostList.vue
@@ -4,24 +4,11 @@
       <thead>
         <tr>
           <th
+            v-for="(category, idx) in categories"
+            :key="idx"
             class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
           >
-            id
-          </th>
-          <th
-            class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
-          >
-            제목
-          </th>
-          <th
-            class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
-          >
-            게시일
-          </th>
-          <th
-            class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
-          >
-            분류태그
+            {{ category }}
           </th>
         </tr>
       </thead>
@@ -29,54 +16,49 @@
         <Post
           v-for="post in paginatedPosts"
           :key="post.id"
-          :post="post"
-          :isLoading="isLoading"
+          @goToPrev="goToPrev"
+          @goToNext="goToNext"
         />
       </tbody>
     </table>
-    <Pagination :goToPrev="goToPrev()" :goToNext="goToNext()" />
+    <Pagination
+      @currentPage="currentPage"
+      :totalPages="totalPages"
+      :paginatedPosts="paginatedPosts"
+    />
   </div>
 </template>
 <script setup>
-import Post from './Post.vue';
-import Pagination from './Pagination.vue';
-import { ref, onMounted } from 'vue';
+import Post from "./Post.vue";
+import Pagination from "./Pagination.vue";
+import { ref } from "vue";
+import { defineProps, defineEmits } from "vue";
+const props = defineProps(["fileteredPosts"]);
+const { fileteredPosts } = toRefs(props);
 
+const categories = ["id", "제목", "게시일", "분류태그"];
+const emit = defineEmits(["goToPrev", "goToNext"]);
 const currentPage = ref(1);
 const perPage = 10;
-const posts = ref([]);
-const isLoading = ref(false);
-const totalPages = computed(() => Math.ceil(posts.value.length / perPage));
+const totalPages = computed(() =>
+  Math.ceil(fileteredPosts.value.length / perPage)
+);
 const paginatedPosts = computed(() => {
   const startIndex = (currentPage.value - 1) * perPage;
   const endIndex = currentPage.value * perPage;
-  return posts.value.slice(startIndex, endIndex);
+  return fileteredPosts.value.slice(startIndex, endIndex);
 });
-const fetchPosts = async () => {
-  try {
-    isLoading.value = true;
-    const response = await fetch('http://localhost:3000/posts');
-    if (!response.ok) {
-      throw Error('⚠️ 데이터를 가져올 수 없습니다!');
-    }
-    posts.value = await response.json();
-    isLoading.value = false;
-  } catch (error) {
-    console.error('데이터 로딩 중 에러:', error);
-    isLoading.value = false;
-  }
-};
+
 const goToPrev = () => {
-  if (currentPage.value > 1) {
-    currentPage.value--;
+  if (currentPage > 1) {
+    emit("update-currentPage", currentPage - 1);
   }
 };
 
 const goToNext = () => {
-  if (currentPage.value < totalPages.value) {
-    currentPage.value++;
+  if (currentPage < totalPages) {
+    emit("update-currentPage", currentPage + 1);
   }
 };
-onMounted(fetchPosts);
 </script>
 <style></style>
