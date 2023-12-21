1dfb46658aea1683b749888c6646c38808e5accd
jinlijinyoung <shlee@idevel.kr>
2023-12-21 18:22:26 +0900

    Style: 불필요 코드 수정

--- utils/getPosts.js
+++ utils/getPosts.js
@@ -1,12 +1,11 @@
 import { ref } from "vue";
-import axios from "axios";
 
 const getPosts = () => {
   const posts = ref([]);
   const error = ref(null);
   const load = async () => {
     try {
-      let response = await useFetch("http://localhost:3000/posts");
+      let response = await fetch("http://localhost:3000/posts");
       if (!response.ok) {
         throw Error("⚠️ 데이터를 읽어올 수 없습니다!");
       }
