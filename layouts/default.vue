1fc08dd8e4e002186bfef9e00354e3cff162b722
jinlijinyoung <shlee@idevel.kr>
2023-12-22 18:50:24 +0900

    Refactor: 레이아웃 설정

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <Header />
+  <div
+    class="bg-gray-100 flex flex-col items-center justify-center h-screen px-2 py-4"
+  >
+    <div class="flex flex-col w-full lg:w-1/2 h-full bg-white px-9 py-3">
+      <slot />
+    </div>
+  </div>
+</template>
+<script setup>
+import Header from "~/components/Header.vue";
+</script>
+<style></style>
