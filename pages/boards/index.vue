b894b3a5e75e47fcbdd32cf761ff4c5c668f50bd
jinlijinyoung <shlee@idevel.kr>
2023-12-22 18:52:03 +0900

    Refactor: 코드 리팩토링

--- pages/boards/index.vue
+++ pages/boards/index.vue
@@ -1,87 +1,69 @@
 <template>
-  <div
-    class="bg-gray-100 flex flex-col items-center justify-center h-screen px-2 py-4"
-  >
-    <div class="flex flex-col w-full lg:w-1/2 h-full bg-white px-9">
-      <div class="w-auto flex justify-between items-center p-3">
-        <h2 class="text-xl font-semibold">공지사항</h2>
-        <button
-          @click="moveToCreatePage"
-          class="flex items-center bg-lime-500 text-white font-semibold py-2 px-4 rounded-md transition-colors duration-300"
+  <div class="w-auto flex justify-between items-center p-3">
+    <h2 class="text-xl font-semibold">공지사항</h2>
+    <button
+      @click="moveToCreatePage"
+      class="flex items-center bg-lime-500 text-white font-semibold py-2 px-4 rounded-md transition-colors duration-300"
+    >
+      <p class="text-white">글쓰기</p>
+    </button>
+  </div>
+
+  <div class="w-full flex justify-center p-1 mb-4">
+    <div class="relative w-full">
+      <input
+        v-model="searchText"
+        type="text"
+        class="w-full bg-white py-2 pl-10 pr-4 rounded-lg focus:outline-none border-2 border-gray-100 focus:border-black transition-colors duration-300"
+      />
+      <div
+        class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none"
+      >
+        <svg
+          class="w-4 h-4 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 20 20"
         >
-          <p class="text-white">글쓰기</p>
-        </button>
-      </div>
-      <div class="w-full flex justify-center p-1 mb-4">
-        <div class="relative w-full">
-          <input
-            v-model="searchText"
-            type="text"
-            class="w-full bg-white py-2 pl-10 pr-4 rounded-lg focus:outline-none border-2 border-gray-100 focus:border-black transition-colors duration-300"
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
           />
-          <div
-            class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none"
-          >
-            <svg
-              class="w-4 h-4 text-gray-800 dark:text-white"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 20 20"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-              />
-            </svg>
-          </div>
-        </div>
+        </svg>
       </div>
-      <PostList :filteredPosts="filteredPosts" />
     </div>
   </div>
+  <PostList :searchText="searchText" :totalPages="totalPages" />
 </template>
 <script setup>
-import PostList from '~/components/PostList.vue';
-import { ref, computed } from 'vue';
+import PostList from "~/components/PostList.vue";
+import { ref } from "vue";
 
-const posts = ref([]);
-const searchText = ref('');
-const isLoading = ref(false);
+const searchText = ref("");
+const error = ref(null);
+const totalPages = ref([]);
 
-const fetchPosts = async () => {
+const load = async () => {
   try {
-    isLoading.value = true;
-    const response = await fetch('http://localhost:3000/posts', {
-      method: 'GET',
+    let response = await fetch("http://localhost:3000/boards", {
+      method: "GET",
     });
     if (!response.ok) {
-      throw Error('⚠️ 데이터를 가져올 수 없습니다!');
+      throw Error("⚠️ 데이터를 읽어올 수 없습니다!");
     }
-    posts.value = await response.json();
-    isLoading.value = false;
-  } catch (error) {
-    console.error('데이터 로딩 중 에러:', error);
-    isLoading.value = false;
+    totalPages.value = await response.json();
+  } catch (err) {
+    error.value = err.message;
+    console.error(err);
   }
 };
-fetchPosts();
-const filteredPosts = computed(() => {
-  // computed를 사용하면 캐싱기능 존재
-  if (searchText.value) {
-    return posts.value.filter((post) => {
-      return post.title && post.title.includes(searchText.value);
-    });
-  } else {
-    return posts.value;
-  }
-});
-
+load();
 const moveToCreatePage = () => {
-  navigateTo('/boards/create');
+  return navigateTo("/boards/create");
 };
 </script>
 <style></style>
