6adf5d81ff077c8f1ce13feb88a026b4f799db08
jinlijinyoung <shlee@idevel.kr>
2023-12-22 18:49:07 +0900

    Fix: pagination 기능 수정

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -3,11 +3,12 @@
     class="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between"
   >
     <span class="text-xs xs:text-sm text-gray-900">
-      {{ currentPage }} / {{ totalPages }}
+      {{ props.currentPage }} / {{ props.totalPages }}
     </span>
     <div class="inline-flex mt-2 xs:mt-0">
       <button
         @click="goToPrev"
+        :disabled="props.currentPage === 1"
         class="text-sm text-indigo-50 transition duration-150 hover:bg-lime-600 bg-lime-500 font-semibold py-2 px-4 rounded-l"
       >
         ﹤
@@ -15,6 +16,7 @@
       &nbsp; &nbsp;
       <button
         @click="goToNext"
+        :disabled="props.currentPage === props.totalPages"
         class="text-sm text-indigo-50 transition duration-150 hover:bg-lime-600 bg-lime-500 font-semibold py-2 px-4 rounded-r"
       >
         ﹥
@@ -23,8 +25,17 @@
   </div>
 </template>
 <script setup>
-import { defineProps, defineEmits } from 'vue';
-const { currentPage, totalPages } = defineProps(['currentPage', 'totalPages']);
-const emit = defineEmits(['goToPrev', 'goToNext']);
+import { defineProps, defineEmits } from "vue";
+
+const props = defineProps(["current-page", "total-pages"]);
+const emit = defineEmits();
+
+const goToPrev = () => {
+  emit("go-to-prev");
+};
+
+const goToNext = () => {
+  emit("go-to-next");
+};
 </script>
 <style></style>
