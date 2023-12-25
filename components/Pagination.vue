1f8ebcf3669de32d59fe10ca3c9bf913078dd3a2
eeseohyun <jinlijinyoung@gmail.com>
2023-12-26 06:11:16 +0900

    Fix: 기능 수정

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -3,12 +3,11 @@
     class="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between"
   >
     <span class="text-xs xs:text-sm text-gray-900">
-      {{ props.currentPage }} / {{ props.totalPages }}
+      {{ currentPage }} / {{ totalPages }}
     </span>
     <div class="inline-flex mt-2 xs:mt-0">
       <button
         @click="goToPrev"
-        :disabled="props.currentPage === 1"
         class="text-sm text-indigo-50 transition duration-150 hover:bg-lime-600 bg-lime-500 font-semibold py-2 px-4 rounded-l"
       >
         ﹤
@@ -16,7 +15,6 @@
       &nbsp; &nbsp;
       <button
         @click="goToNext"
-        :disabled="props.currentPage === props.totalPages"
         class="text-sm text-indigo-50 transition duration-150 hover:bg-lime-600 bg-lime-500 font-semibold py-2 px-4 rounded-r"
       >
         ﹥
@@ -25,17 +23,28 @@
   </div>
 </template>
 <script setup>
-import { defineProps, defineEmits } from "vue";
+import { defineProps } from 'vue';
+import { useRouter } from 'vue-router';
 
-const props = defineProps(["current-page", "total-pages"]);
-const emit = defineEmits();
+const router = useRouter();
+
+const { currentPage, totalPages } = defineProps([
+  'current-page',
+  'total-pages',
+]);
 
 const goToPrev = () => {
-  emit("go-to-prev");
+  return router.push({
+    params: { page: currentPage > 1 ? currentPage - 1 : 1 },
+  });
 };
 
 const goToNext = () => {
-  emit("go-to-next");
+  return router.push({
+    params: {
+      page: totalPages >= currentPage ? currentPage + 1 : totalPages,
+    },
+  });
 };
 </script>
 <style></style>
