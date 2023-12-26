0fab0d07346d7c65c1a7f470220180b3b23979f5
jinlijinyoung <shlee@idevel.kr>
2023-12-26 13:40:45 +0900

    Style: 코드 수정

--- components/PostList.vue
+++ components/PostList.vue
@@ -16,35 +16,29 @@
         <Post v-for="(post, idx) in paginatedData" :post="post" :key="idx" />
       </tbody>
     </table>
-    <Pagination
-      :current-page="pagination.currentPage"
-      :total-pages="Math.ceil(totalPages.length / limit)"
-    />
   </div>
 </template>
 <script setup>
-import Post from './Post.vue';
-import Pagination from './Pagination.vue';
-import { ref, defineProps } from 'vue';
-import { useRoute } from 'vue-router';
+import Post from "./Post.vue";
+import { ref, defineProps } from "vue";
+import { useRoute } from "vue-router";
 
-const { searchText, totalPages } = defineProps(['searchText', 'totalPages']);
+const { searchText } = defineProps(["searchText"]);
+
 const route = useRoute();
-const categories = ['id', '제목', '게시일', '분류태그'];
-const limit = 10;
+const categories = ["id", "제목", "게시일", "분류태그"];
 const pagination = ref({
-  currentPage: +route.params.page,
-  searchText: '',
+  currentPage: route.params.page,
+  searchText: searchText,
 });
-
 const paginatedData = ref([]);
 
 const fetchData = async () => {
   try {
     const response = await fetch(
-      `http://localhost:3000/boards?_page=${pagination.value.currentPage}&_limit=${limit}`,
+      `http://localhost:3000/boards?_page=${pagination.value.currentPage}&_limit=10`,
       {
-        method: 'GET',
+        method: "GET",
       }
     );
     const data = await response.json();
