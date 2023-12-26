7c69ee31384ffb6400fb1c87387893d9d1ec2095
이서현 <shlee@idevel.kr>
2023-12-26 17:34:14 +0900

    Feat: host설정

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/ui'],
-  css: ['~/assets/css/main.css'],
+  modules: ["@nuxt/ui"],
+  css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
+  },
+  devServer: {
+    port: 3001,
+    host: "0.0.0.0",
   },
 });
