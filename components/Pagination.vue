d9ee1e0b6c581aaebccdaad1a25f00f380f02281
jinlijinyoung <shlee@idevel.kr>
2023-12-26 13:40:02 +0900

    Fix: 기능 수정

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -3,11 +3,12 @@
     class="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between"
   >
     <span class="text-xs xs:text-sm text-gray-900">
-      {{ currentPage }} / {{ totalPages }}
+      {{ currentPage }} / {{ totalPagesNum }}
     </span>
     <div class="inline-flex mt-2 xs:mt-0">
       <button
         @click="goToPrev"
+        ::disabled="currentPage === 1"
         class="text-sm text-indigo-50 transition duration-150 hover:bg-lime-600 bg-lime-500 font-semibold py-2 px-4 rounded-l"
       >
         ﹤
@@ -15,6 +16,7 @@
       &nbsp; &nbsp;
       <button
         @click="goToNext"
+        :disabled="currentPage === totalPagesNum"
         class="text-sm text-indigo-50 transition duration-150 hover:bg-lime-600 bg-lime-500 font-semibold py-2 px-4 rounded-r"
       >
         ﹥
@@ -23,28 +25,27 @@
   </div>
 </template>
 <script setup>
-import { defineProps } from 'vue';
-import { useRouter } from 'vue-router';
+import { defineProps } from "vue";
+import { useRouter, useRoute } from "vue-router";
 
 const router = useRouter();
-
-const { currentPage, totalPages } = defineProps([
-  'current-page',
-  'total-pages',
-]);
+const route = useRoute();
+let currentPage = route.params.page;
+const { totalPages } = defineProps(["total-pages"]);
+const totalPagesNum = Math.ceil(totalPages / 10);
 
 const goToPrev = () => {
-  return router.push({
-    params: { page: currentPage > 1 ? currentPage - 1 : 1 },
-  });
+  const newPage = +currentPage > 1 ? +currentPage - 1 : 1;
+  return router.push({ params: { page: newPage } });
 };
 
 const goToNext = () => {
-  return router.push({
-    params: {
-      page: totalPages >= currentPage ? currentPage + 1 : totalPages,
-    },
-  });
+  const newPage =
+    +currentPage <= +totalPagesNum - 1 ? +currentPage + 1 : +totalPagesNum;
+  console.log(newPage);
+  console.log(currentPage);
+  console.log("totalPages", totalPagesNum);
+  return router.push({ params: { page: newPage } });
 };
 </script>
 <style></style>
