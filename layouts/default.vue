5836a7bedeebfa0c9f97059f1332499e56ba3cce
jinlijinyoung <shlee@idevel.kr>
2023-12-26 13:42:05 +0900

    Style: Style 수정

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <Header />
   <div
-    class="bg-gray-100 flex flex-col items-center justify-center h-full px-2 py-4"
+    class="bg-gray-100 flex flex-col items-center justify-center min-h-full px-2 py-4"
   >
     <div class="flex flex-col w-full lg:w-1/2 h-full bg-white px-9 py-3">
       <slot />
@@ -9,6 +9,6 @@
   </div>
 </template>
 <script setup>
-import Header from '~/components/Header.vue';
+import Header from "~/components/Header.vue";
 </script>
 <style></style>
